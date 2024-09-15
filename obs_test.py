ccef5b7e4c2300d887724c49d771ddbf6246d712
zzw168 <78839407@qq.com>
2024-09-15 19:24:50 +0400

    OBS

--- obs_test.py
+++ obs_test.py
@@ -1,6 +1,6 @@
 """
     pip install obsws-python
-    OBS WebSocket 密码： D9AQaWOH0b8Tu0Xn
+    用法备注： .imagedata -> .image_data 每个命令的词之间加 _(下划线)，且字母都为小写
 """
 import base64
 import obsws_python as obs
@@ -25,13 +25,19 @@ if __name__ == '__main__':
     print(f"OBS Version: {resp.obs_version}")
 
     # SetCurrentProgramScene
-    cl.set_current_program_scene("终点")  # 切换终点场景
+    # cl.set_current_program_scene("终点")  # 切换终点场景
 
     # 查询返回json数据
-    resp = cl.send("GetVersion", raw=True)
-    print(f"response data: {resp}")
+    # resp = cl.send("GetVersion", raw=True)
+    # print(f"response data: {resp}")
     # resp = cl.create_scene('第一视图')
     # resp = cl.remove_scene('第一视图')
-    resp = cl.get_source_screenshot('终点', "jpg", None, None, 100)
-    print(resp.image_data)
-    strToImage(resp.image_data, './a.jpg')
+    # resp = cl.get_source_screenshot('终点', "jpg", None, None, 100)
+    # print(resp.image_data)
+    # strToImage(resp.image_data, './a.jpg')
+    resp = cl.get_scene_item_list('现场')
+    print(resp.scene_items[0])
+    for item in resp.scene_items:
+        print(item['sourceName'])
+
+    cl.set_scene_item_enabled('现场', 8, True)  # 打开视频来源
